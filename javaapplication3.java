25d01e342a12d5ade2c3373d6b3f7e8978e31443
rahma <rahmaelwakiel@gmail.com>
2023-12-20 13:25:30 -0800

    build & deploy to docker

--- javaapplication3.java
+++ javaapplication3.java
@@ -5,7 +5,7 @@ public class Rahma {
 
    
     public static void main(String[] args) {
-        System.out.println("rahma mohammed elwakiel")
+        System.out.println("rahma mohammed elwakiel");
     }
     
 }
